59745f0e74c19253fbe0a7cf39672af8578c5bda
Olga Melnikova <omelnikova@accesssoftek.com>
2021-06-29 17:56:42 +0400

    task2 partially completed

---
+++ task2/scripts/Productnfo.js
@@ -0,0 +1,134 @@
+
+const featuredItemElement = document.querySelector('.featured');
+
+/**
+ * Class describes Product object
+ * @param {number} id productID
+ * @param {string} img image file name
+ * @param {string} name name
+ * @param {number} price price
+ * @param {string} description discription
+ *
+ */
+class ProductInfo {
+    constructor(id, img, name, price, description) {
+        this.id = id;
+        this.img = img;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
+
+class ProductList {
+    constructor() {
+        this.products = [];
+    }
+    fetchProducts() {
+        this.products = [
+            new ProductInfo(
+                0,
+                '0.jpg',
+                'Mango People T-shirt 1',
+                100.99,
+                'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+            ),
+            new ProductInfo(
+                1,
+                '1.jpg',
+                'Mango People T-shirt 2',
+                99.99,
+                'Consectetue Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+            ),
+            new ProductInfo(
+                2,
+                '2.jpg',
+                'Mango People T-shirt 3',
+                9.99,
+                'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. ',
+            ),
+            new ProductInfo(
+                3,
+                '0.jpg',
+                'Product 4',
+                100.99,
+                'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula',
+            ),
+            new ProductInfo(
+                4,
+                '1.jpg',
+                'Product 5',
+                99.99,
+                'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+            ),
+            new ProductInfo(
+                5,
+                '2.jpg',
+                'Product 6',
+                9.99,
+                'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. ',
+            ),
+        ]
+    }
+
+    /**
+     * @param {ProductInfo[]} products products array
+     * @param {HTMLDivElement} featuredItemElement element to insert product item inside
+     */
+    insertCardItems(products, featuredItemElement) {
+        let productCard = '';
+        for (let item of products) {
+            productCard += createProductCardMarkup(item);
+        }
+        featuredItemElement.insertAdjacentHTML('afterbegin', productCard);
+    }
+}
+
+
+/**
+* @param {ProductInfo} product from products array
+* @returns {string} html markup for creating product card
+*/
+
+function createProductCardMarkup(product) {
+    return `
+
+    <div class="featured_item">
+             <a class="image_featured" href="#">
+                <img class="image_item" src="img/${product.img}" alt="product">
+                <div class="featured_name_price">
+                    <p class="featured_item_text">${product.name}</p>
+                    <p class="featured_item_text small"> ${product.description}</p>
+                    <p class="featured_item_price">$${product.price}</p>
+                </div>
+            </a>
+            <div class="to_cart" data-prod_id ="${product.id}">
+                <a class="button_to_cart" href="#">
+                    <img src="img/Cart_white.png" alt="cart_white">
+                    <p class="to_cart_text">Add to Cart</p>
+                </a>
+            </div>
+        </div>
+    `;
+}
+
+
+let productList = new ProductList();
+productList.fetchProducts();
+productList.insertCardItems(productList.products, featuredItemElement);
+
+/**
+ * EventListener on 'click' for all elements "Add to Cart" for each Product Item found on the page.
+ * AFinally calls function addToShopping cart from Shoppingcart.js to create shopping cart markup.
+ */
+
+function AddEventListenerOnClickAddToCart() {
+    let allAddToCartElements = document.querySelectorAll('.to_cart');
+    allAddToCartElements.forEach(function (element) {
+        element.addEventListener('click', function (event) {
+            const prodID = event.currentTarget.getAttribute('data-prod_id');
+            addProductToShoppingcart(prodID);
+        })
+    })
+}
+AddEventListenerOnClickAddToCart();
---
+++ task2/scripts/Shoppingcart.js
@@ -0,0 +1,98 @@
+
+const addToCart = document.querySelector('.to_cart');
+const cart = document.querySelector('.cart');
+const shoppingcart = document.querySelector('.shoppingcart');
+const counter = document.querySelector('.circle');
+const totalCart = document.querySelector('.total_cart');
+const totalCartSum = document.querySelector('.counter_total');
+
+cart.addEventListener('click', function () {
+    shoppingcart.classList.toggle('none');
+})
+
+/**
+ * Class describes Shoppingcart Item object
+ *
+ */
+class shoppingcartItem {
+    constructor(price, quantity) {
+        this.price = price;
+        this.quantity = quantity;
+
+    }
+}
+/**
+ * Class describes Shoppingcart object
+ * 
+ */
+class Shoppingcart {
+
+    constructor() {
+        this.items = [];
+    }
+
+    /**
+    * Add element with quantity = 1 to array
+    * @param {number} pruductID product ID
+    */
+    addItem(productID) {
+        this.items.push(new shoppingcartItem(
+            productList.products[productID].price,
+            1,
+        ))
+    }
+
+    /**
+   * Create markup for each added item in the shopping cart table 
+   * @param {number} pruductID pruductID product ID
+   */
+    renderNewItem(productID) {
+        let productInRow = `
+        <div class="table_row">
+            <div>${productList.products[productID].name}</div>
+            <div>
+                <span class="items_quantity" data-prod_id="${productID}">1</span>
+            </div>
+            <div>$${productList.products[productID].price}</div>
+            <div>
+                $<span class="items_price" data-prod_id="${productID}">${productList.products[productID].price}</span>
+            </div>
+        </div>
+    `;
+        totalCart.insertAdjacentHTML("beforebegin", productInRow);
+    }
+    /**
+       * Check if item already exists - increase Quantity, otherwise add new Item and increase counter in circle
+       * @param {number} pruductID pruductID product ID
+       */
+    adjustProductQuantity(productID) {
+        let itemInBasket = document.querySelector(`.items_quantity[data-prod_id="${productID}"]`);
+        if (itemInBasket) {
+            itemInBasket.textContent++;
+
+        } else {
+            shopcart.renderNewItem(productID);
+            counter.textContent++;
+        }
+
+    }
+
+    /**
+       * Calculate total shopping cart amount
+       */
+    calculateTotal() {
+        let totalSum = 0;
+        for (let i = 0; i < shopcart.items.length; i++) {
+            totalSum += shopcart.items[i].price * shopcart.items[i].quantity;
+        }
+        totalCartSum.textContent = totalSum.toFixed(1);
+    }
+}
+
+let shopcart = new Shoppingcart();
+
+function addProductToShoppingcart(productID) {
+    shopcart.addItem(productID);
+    shopcart.adjustProductQuantity(productID);
+    shopcart.calculateTotal(productID);
+}
